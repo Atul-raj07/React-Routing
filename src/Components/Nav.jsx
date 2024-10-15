4a9a2d01c8071e0912de495ad3a1e4786bbb6cce
Atul-raj07 <atulrajgupta12j@gmail.com>
2024-10-15 19:06:47 +0530

    routing-with Navlinks

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <div className='flex items-center gap-10 bg-red-600'>
-        <Link  to="/">home</Link>
-        <Link to="/about">about</Link>
-        <Link to="/services">service </Link>
-        <Link to="/products">products</Link>
+        <NavLink style={(e)=> e.isActive ? {textDecoration:"underline"}:{}} className={(e)=>`${e.isActive ? "text-yellow-500":""}`} to="/">home</NavLink>
+        <NavLink style={(e)=> e.isActive ? {textDecoration:"underline"}:{}} className={(e)=>`${e.isActive ? "text-yellow-500":""}`} to="/about">about</NavLink>
+        <NavLink style={(e)=> e.isActive ? {textDecoration:"underline"}:{}} className={(e)=>`${e.isActive ? "text-yellow-500":""}`} to="/services">service </NavLink>
+        <NavLink style={(e)=> e.isActive ? {textDecoration:"underline"}:{}} className={(e)=>`${e.isActive ? "text-yellow-500":""}`} to="/products">products</NavLink>
     </div>
   )
 }
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Services = () => {
+  const navigate = useNavigate()
+  const submitHandler = (e) => {
+    e.preventDefault()
+console.log("lalalal");
+navigate("/")
+// navigate(-1)
+  }
   return (
-    <div>Services ka page free services</div>
+    <div>
+      <h1>services </h1>
+      <form onSubmit={submitHandler}>
+      <button >suvbmiit</button>
+      </form>
+    </div>
   )
 }
 
